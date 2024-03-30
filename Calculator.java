a9652f9755c716cdc08b28f6f2dfa141683c98b2
Zarihan <Kersanel@gmail.com>
2024-03-30 08:13:37 +0300

    add body program

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,155 @@
 package Dz_Calculator;
 
-public class Calculator {
+import java.util.Scanner;
+ 
+interface Operation{
+    double exec( double valueOne, double valueTwo );
+  } 
 
+class OpPlus implements Operation{
+ 
+    @Override
+    public double exec(double valueOne, double valueTwo ) {
+        return (valueOne + valueTwo );
+    }
+}
+
+class OpMinus implements Operation{
+ 
+    @Override
+    public double exec(double valueOne, double valueTwo ) {
+        return (valueOne - valueTwo );
+    }
+}
+
+class OpMultiply implements Operation{
+ 
+    @Override
+    public double exec(double valueOne, double valueTwo ) {
+        return (valueOne * valueTwo );
+    }
+}
+
+class OpDivision implements Operation{
+    @Override
+    public double exec(double valueOne, double valueTwo ) {
+        try{
+            double p = valueOne / valueTwo;
+        }catch(ArithmeticException e){
+            System.out.println(e + " арифметичесская ошибка");
+        }
+        return (valueOne / valueTwo );
+    }
+}
+
+interface OperationFactory
+   {
+     Operation getOpInstance( int op );
+   } 
+
+class MyOpFactory implements OperationFactory{
+ 
+    int operationCode;
+    private Operation operation;
+    
+    @Override
+
+    public Operation getOpInstance(int op) {
+        this.operationCode = op;
+        switch (operationCode){
+            case 0:{
+                operation = new OpPlus();
+                break;
+            }
+            case 1:{
+                operation = new OpMinus();
+                break;
+            }
+            case 2:{
+                operation = new OpMultiply();
+                break;
+            }
+            case 3:{
+                operation = new OpDivision();
+                break;
+            }
+            default:{
+                operation = null;
+            }
+        }
+        return operation;
+    }
+}
+
+public class Calculator {
+    
+    MyOpFactory myOpFactory;
+    
+    public Calculator(MyOpFactory myOpFactory){
+        this.myOpFactory = myOpFactory;
+    }
+
+    public void exec(){
+                
+        boolean nextStep = true;                
+        boolean p = true;
+        double buf = 0;
+        Scanner in = new Scanner(System.in);    
+        do {            
+            
+            double arg1;
+            arg1 = buf;   
+            if(p){        
+                System.out.println("Введите первое число");
+                try{
+                if(!in.hasNextDouble()) {
+                    throw new NumberFormatException();
+                    } else 
+                        arg1 = in.nextDouble();                                                        
+                }catch(NumberFormatException e){
+                    System.out.println(e);   
+                    break;                  
+                }                    
+            }            
+            
+            System.out.println("Выберите операцию:");
+            System.out.print("0 - Сложение\t");  
+            System.out.print("1 - Вычитание\t");
+            System.out.print("2 - Умножение\t");
+            System.out.print("3 - Деление");
+            System.out.println();
+            int operation = in.nextInt();
+            
+            
+            System.out.println("Введите второе число");
+            double arg2 = in.nextDouble();
+ 
+            
+            System.out.print("Результат равен:\t");
+            Operation op = myOpFactory.getOpInstance(operation);
+            if (op != null){
+                System.out.println(op.exec(arg1, arg2));
+                buf = (int) op.exec(arg1, arg2);
+            }
+            else System.out.println("Недопустимая операция!!!");
+            
+            System.out.println("Хотите использовать результат как первое число?");
+            System.out.print("1 - Да\t");
+            System.out.println("2 - Нет");
+            int what = in.nextInt();
+            if (what == 1){
+                p = false;
+                continue;
+            }
+            else p = true;
+            
+           
+            System.out.println("Хотите продолжить?");
+            System.out.print("1 - Продолжить\t");
+            System.out.println("2 - Выйти");
+            int whatDo = in.nextInt();
+            if (whatDo == 2) 
+                nextStep = false;
+        } while (nextStep);
+    }
 }
--- CalculatorFabrika.java
+++ CalculatorFabrika.java
@@ -1,5 +1,11 @@
 package Dz_Calculator;
 
 public class CalculatorFabrika {
+    public static void main(String[]args){
 
+        MyOpFactory myOpFactory = new MyOpFactory();
+        Calculator calc = new Calculator(myOpFactory);
+
+        calc.exec();
+    }
 }
